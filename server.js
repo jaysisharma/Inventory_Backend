bda7bbfe85c0c445c5095e00666c1b52d275a1f7
jaysi <jaysi@kali.jaysi>
2025-01-13 09:34:26 +0530

    first-commit

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ import secondHandProductRouter from './routes/secondHandProductRoutes.js';
 import cors from 'cors';
 // Initialize App
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({ origin: 'https://mediumvioletred-buffalo-504320.hostingersite.com/' }));
 
 app.use(bodyParser.json());
 dotenv.config();
